61ccdbae5da09bfefb245d4e077c29bb30f0d341
Itamar Syn-Hershko <itamar@code972.com>
2017-07-09 13:15:59 +0300

    Pass the ForbiddenAPIs check

--- src/main/java/com/code972/elasticsearch/HebrewAnalysisPlugin.java
+++ src/main/java/com/code972/elasticsearch/HebrewAnalysisPlugin.java
@@ -18,6 +18,7 @@ import org.apache.lucene.analysis.Analyzer;
 import org.elasticsearch.SpecialPermission;
 import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
 import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.common.SuppressForbidden;
 import org.elasticsearch.common.settings.ClusterSettings;
 import org.elasticsearch.common.settings.IndexScopedSettings;
 import org.elasticsearch.common.settings.Settings;
@@ -142,6 +143,7 @@ public final class HebrewAnalysisPlugin extends Plugin implements ActionPlugin, 
         }
 
         @Override
+        @SuppressForbidden(reason = "Paths are loaded using Environment so are safe to use")
         public DictHebMorph run() {
             if (Files.exists(Paths.get(path))) {
                 try {
